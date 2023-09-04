85ef03fcc752db8642cc6a49b3543e312cd07124
gaurav-shewale-0104 <gauravshewale0104@gmail.com>
2023-09-04 22:46:51 +0530

    changes added in main.py

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ def myfun(string_with_data):
     #convert string data into lowercase
     string_with_data=string_with_data.lower()
     return string_with_data
+string = "Python Is High Level Interpreter Language."
+
+string = myfun(string)
